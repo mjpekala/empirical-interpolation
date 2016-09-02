113ccabe7b4845f09151b308e7eb29ca1c4e36c0
mjpekala <mike.pekala@jhuapl.edu>
2016-09-02 10:59:21 -0400

    ported over the code that constructs W_n.

--- src/make_domain_2d.m
+++ src/make_domain_2d.m
@@ -1,4 +1,4 @@
-function [Omega, U_] = make_domain_2d(n_max, delta)
+function [Omega, W_n] = make_domain_2d(n_max, delta)
 % MAKE_DOMAIN_2D   Creates example spatial and interpolation domains.
 %
 %  Creates a simple 2d domain (compact subset of R^2) and a polynomial
@@ -6,25 +6,42 @@ function [Omega, U_] = make_domain_2d(n_max, delta)
 %  domain (e.g. triangular, hexoganal, higher-dimensional) or a
 %  different function space (e.g. Legandre polynomials) you'll need
 %  different code.
+% 
+%  The polynomial space we create is the following:
+%     W_n(Omega) := { (x^i)*(y^j), (x,y) \in Omega, i+j <= n },  0 <= n <= n_max
+%  which has m = (n+1)(n+2)/2 elements.
 %
+%  Parameters:
 %     n_max := the maximum polynomial degree to use when
-%              constructing U_.
+%              constructing W_n.
 %     delta := resolution of the interpolation domain.
+%
+%  References:
+%    Maday et al. "A general multipurpose interpolation procedure:
+%                  the magic points," CPAA 2009.
  
 if nargin < 2, delta=.1; end
+assert(delta > 0);
+assert(n_max > 1);
+
+m = (n_max+1)*(n_max+2) / 2;   
 
 x = -1:delta:1;
 [X,Y] = meshgrid(x, x);
 Omega = [X(:) Y(:)];
 
-% Construct the polynomial basis:
-%
-%    W_n(Omega) := { (x^i)*(y^j), (x,y) \in Omega, i+j <= n },  0 <= n <= n_max
-%
-% with m = (n+1)(n+2)/2 elements.
-%
-m = (n_max+1)*(n_max+2) / 2;   
-U_ = cell(m,1);  % underscore is my notation indicating this is not a matrix
+
+% the code below is just a quick way to enumerate the space:
+%   ii+jj <= n      for 0 <= n <= n_max
+Tmp = tril(ones(n_max+1,n_max+1));
+[ii,jj] = ind2sub(size(Tmp), find(Tmp));
+jj = jj - 1;
+ii = n_max+1-ii;
+assert(length(ii) == m);
+clear Tmp;
+
+
+W_n = cell(m,1);  % underscore is my notation indicating this is not a matrix
 for kk = 1:m
-    U_{kk} = @(X2d)  X2d(:,1).^ii(kk) .* X2d(:,2).^jj(kk);
+    W_n{kk} = @(X2d)  X2d(:,1).^ii(kk) .* X2d(:,2).^jj(kk);
 end
