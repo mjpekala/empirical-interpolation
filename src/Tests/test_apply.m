564fc356810f9b984d2ad3b4db279885ed295371
mjpekala <mike.pekala@jhuapl.edu>
2016-09-02 13:09:11 -0400

    added

---
+++ src/Tests/test_apply.m
@@ -0,0 +1,18 @@
+% TEST_APPLY
+
+addpath('..');
+
+f = {};
+f{1} = @(X) X(:,1) + X(:,2);
+f{2} = @(X) X(:,1).*X(:,2);
+f{3} = @(X) X(:,1).^2 + X(:,2).^2;
+
+X = [1 1 ; 2 2; 3 3];
+
+Z = apply(f, X);
+
+for ii = 1:length(f)
+    assert(all(Z(:,ii) == f{ii}(X)));
+end
+
+
