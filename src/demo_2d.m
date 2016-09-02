f20be9fd8b5ac554629e3f328ba16285a3d04023
mjpekala <mike.pekala@jhuapl.edu>
2016-09-02 13:23:03 -0400

    added some diagnostic plots

--- src/demo_2d.m
+++ src/demo_2d.m
@@ -4,12 +4,14 @@
 %   Maday et al. "A general multipurpose interpolation procedure:
 %                 the magic points," CPAA 2009.
 
-% construct a toy function
+m = 5;
+
+%% construct a toy function
 f = @(X) cos(3*X(:,1)) .* sin(2*X(:,2));
 f_rescaled = @(X) f(X*pi);
 [Omega, U_] = make_domain_2d(5, .02);
 
-% visualize the function of interest and a basis function 
+%% visualize the function of interest 
 Z = f_rescaled(Omega);
 n = sqrt(length(Z));
 X = reshape(Omega(:,1), n, n);
@@ -17,15 +19,21 @@ Y = reshape(Omega(:,2), n, n);
 Z = reshape(Z, n, n);
 figure; surf(X, Y, Z); title('f_true');
 
-U1 = reshape(U_{1}(Omega), n, n);
-figure; surf(X, Y, U1); title('U_1');
 
-% interpolate the function of interest
+%% interpolate the function of interest
 tic
-f_interp = interp_magic(f_rescaled, Omega, U_, 10);
+[f_interp, s] = interp_magic(f_rescaled, Omega, U_, m);
 toc
 
+%% visualize result
 Zhat = reshape(f_interp(Omega), n, n);
 figure; surf(X, Y, Zhat); title('$\hat{f}$', 'interpreter', 'latex');
 
 figure; surf(X, Y, abs(Z-Zhat)), title('error (L2)');
+
+%% Some diagnostics
+for k = 1:m
+    Ui = reshape(s.u{k}(Omega), n, n);
+    figure; surf(X, Y, Ui);  title(sprintf('U_{%d}', k));
+end
+
