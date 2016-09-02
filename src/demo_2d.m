cc4e886e76959f3e6659029ff4f48c70d12fc877
mjpekala <mike.pekala@jhuapl.edu>
2016-09-02 13:02:31 -0400

    added a new plot

--- src/demo_2d.m
+++ src/demo_2d.m
@@ -5,20 +5,23 @@
 %                 the magic points," CPAA 2009.
 
 % construct a toy function
-f = @(x,y) cos(3*x) .* sin(2*y);
-f_rescaled = @(x,y) f(x*pi,y*pi);
+f = @(X) cos(3*X(:,1)) .* sin(2*X(:,2));
+f_rescaled = @(X) f(X*pi);
 [Omega, U_] = make_domain_2d(5, .02);
-Z = f_rescaled(Omega(:,1), Omega(:,2));
 
 % visualize the function of interest and a basis function 
+Z = f_rescaled(Omega);
 n = sqrt(length(Z));
 X = reshape(Omega(:,1), n, n);
 Y = reshape(Omega(:,2), n, n);
 Z = reshape(Z, n, n);
-figure; surf(X, Y, Z); xlabel('x'); ylabel('y');
+figure; surf(X, Y, Z); title('f_true');
 
 U1 = reshape(U_{1}(Omega), n, n);
-figure; surf(X, Y, U1); xlabel('x'); ylabel('y'); title('U_1');
+figure; surf(X, Y, U1); title('U_1');
 
 % interpolate the function of interest
 f_interp = interp_magic(f_rescaled, Omega, U_, 10);
+
+Zhat = reshape(f_interp(Omega), n, n);
+figure; surf(X, Y, Zhat); title('$\hat{f}$', 'interpreter', 'latex');
