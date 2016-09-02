434e6110d2efd36aa1d25f63dbf00b022fd8d9c5
mjpekala <mike.pekala@jhuapl.edu>
2016-09-02 14:21:34 -0400

    updated visualization a bit

--- src/demo_2d.m
+++ src/demo_2d.m
@@ -6,32 +6,36 @@
 
 m = 5;
 
-%% construct a toy function
+%% construct a toy function & interpolate
 f = @(X) cos(3*X(:,1)) .* sin(2*X(:,2));
 f_rescaled = @(X) f(X*pi);
 [Omega, U_] = make_domain_2d(5, .02);
 
-%% visualize the function of interest 
+tic
+[f_interp, s] = interp_magic(f_rescaled, Omega, U_, m);
+toc
+
+%% visualize 
 Z = f_rescaled(Omega);
 n = sqrt(length(Z));
 X = reshape(Omega(:,1), n, n);
 Y = reshape(Omega(:,2), n, n);
 Z = reshape(Z, n, n);
-figure; surf(X, Y, Z); title('f_true');
+
+figure; 
+surf(X, Y, Z); title('f_true');
+hold on;
+stem3(s.x(:,1), s.x(:,2), f_interp(s.x), 'ro');
+hold off;
 
 
-%% interpolate the function of interest
-tic
-[f_interp, s] = interp_magic(f_rescaled, Omega, U_, m);
-toc
-
-%% visualize result
+% the interpolation and error
 Zhat = reshape(f_interp(Omega), n, n);
 figure; surf(X, Y, Zhat); title('$\hat{f}$', 'interpreter', 'latex');
 
 figure; surf(X, Y, abs(Z-Zhat)), title('error (L2)');
 
-%% Some diagnostics
+% some other diagnostics
 for k = 1:m
     Ui = reshape(s.u{k}(Omega), n, n);
     figure; surf(X, Y, Ui);  title(sprintf('U_{%d}', k));
