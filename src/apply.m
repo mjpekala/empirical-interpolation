a5a248466c2e6d97aac50dab5462284888c17cd3
mjpekala <mike.pekala@jhuapl.edu>
2016-09-02 09:50:40 -0400

    updated comments

--- src/apply.m
+++ src/apply.m
@@ -2,16 +2,19 @@ function A = apply(f_cell, X)
 % APPLY Apply an array of functions to one or more inputs.
 %
 %    f_cell := a cell array of k function handles, where each
-%              function maps R^n -> R (i.e. is a scalar-valued function of
-%              n inputs).  It is assumed here that each function
-%              in the array can deal with multiple inputs; the
-%              presumed API is:
+%              function maps R^n -> R (i.e. is a scalar-valued 
+%              function of n inputs).  It is assumed that 
+%              each function in the array can support multiple 
+%              inputs; the presumed API is:
 %
 %                        c = f_cell{ii}(X)
 %
 %              where X is an (m x n) matrix of inputs and c is m x 1.
 %
-%    A      := an (m x k) matrix containing the function evaluations
+%    X      := the set of points that should be evaluated by each f \in f_cell.
+%              a (m x n) matrix of m points each having n dimensions.
+%
+%    A      := an (m x k) matrix containing all the function evaluations.
 
 %  Example:
 %{
