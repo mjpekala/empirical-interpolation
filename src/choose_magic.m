8c53e77d5b99e12a44c8d1ab644c4e2ffe4c2a4d
mjpekala <mike.pekala@jhuapl.edu>
2016-09-05 23:35:29 -0400

    fixed a dumb bug

--- src/choose_magic.m
+++ src/choose_magic.m
@@ -1,5 +1,5 @@
-function [f_interp, s] = interp_magic(Omega, U_, m)
-% INTERP_MAGIC  Chooses "magic points" for interpolation [maday]
+function s = choose_magic(Omega, U_, m)
+% CHOOSE_MAGIC  Chooses "magic points" for interpolation [maday]
 %
 %   Omega : Interpolation domain; a compact subset of R^d.
 %           An (n x d) matrix of n points in d dimensions.
@@ -31,156 +31,63 @@ end
 s.u = zeros(m,1);             % active basis functions; an index into U_
 s.x = zeros(m,1);             % interpolation points, encoded as indices into Omega
 s.sf = zeros(m,1);            % scale factors for the q_i
-s.Q = eye(m);                 % Q_{i,j} := q_j(x_i)
+s.Q = zeros(m);               % Q_{i,j} := q_j(x_i)
 
 % 
 U_all = apply(U_, Omega);     % each u_i evaluated at all points in Omega; (n x b)
-B_all = zeros(m, length(U_)); % the beta coefficients for all u in U_
+Q_all = zeros(n, m);          % rescaled active basis functions
 
-% 
-Q_all = zeros(n, m);          % the ith column of Q_all is q_i for all points in Omega
 
-keyboard % TEMP
 
 %% determine the q_i functions and the interpolation points
 
-% first point is a kind of special case
-[maxval,idx] = max(abs(U_all));
-[r,c] = ind2sub(size(Z), idx);
-s.u(1) = c;
-s.x(1) = r; 
-s.sf(1) = maxval;
+% I.  Choose the first interpolation point
+[s.sf(1), s.x(1), s.u(1)] = ell_inf_2d(U_all);
 Q_all(:,1) = U_all(:,s.u(1)) / s.sf(1);
-
-B_all(1,:) = s.v
-
-for jj = 1:m
-    
-end
+s.Q(1,1) = 1;
 
 
-return % TEMP
+% I_j := Current interpolant of all basis functions 
+%        (using j basis functions).
+beta1 = U_all(s.x(1),:) ./ Q_all(s.x(1),1);
+I_j = bsxfun(@times, beta1, Q_all(:,1)); 
 
+% the active basis functions should interpolate themselves perfectly
+err = abs(U_all(:,s.u(1)) - I_j(:,s.u(1)));
+assert(max(err) < 1e-8);
 
+% II.  Choose the remaining m-1 points
+for jj = 2:m
+    % Choose next basis function and interpolation point
+    [s.sf(jj), s.x(jj), s.u(jj)] = ell_inf_2d(U_all - I_j);
+    Q_all(:,jj) = (U_all(:,s.u(jj)) - I_j(:,s.u(jj))) / s.sf(jj); % rescale max err to 1
 
-
-if 0
-% the first magic point is kind of a special case.
-[s.u{1}, s.x(1,:)] = choose_next_magic_point(U_, Omega, s);
-scale = 1.0 / s.u{1}(s.x(1,:));
-s.q{1} = @(X) s.u{1}(X) / scale;
-
-% compute the remaining magic points and normalized functions
-for k = 2:m
-    fprintf('[%s]: choosing magic point %d (of %d)\n', mfilename, k, m);
-    [u_k, x_k, worst] = choose_next_magic_point(U_, Omega, s);
-    
-    assert(~s.basis(worst));
-    s.basis(worst) = true;
-    
-    I_k = make_interpolant(u_k, s);
-    s.u{k} = u_k;
-    s.x(k,:) = x_k;
-    scale = 1.0 / (u_k(x_k) - I_k(x_k));
-    s.q{k} = @(X) scale*(u_k(X) - I_k(X));
-end
-
-
-% Now that we know the magic points, can build a dedicated function
-% for interpolating v.
-f_interp = make_interpolant(v, s);
-end
-
-
-function I_k = calc_I_k_all(U_all, s)
-% CALC_I_k_ALL  Computes I_k[u] for all u
-%
-k_max = find(s.u == 0, 1, 'first') - 1;
-assert(k_max > 1);
-
-I_k = zeros(size(U_all));
-
-q_1 = U_all(:,s.u(1)) / s.sf(1);
-Q_k
-
-
-
-
-function [u_i, x_i, k_worst] = choose_next_magic_point(U_, Omega, s)
-% CHOOSE_NEXT_MAGIC_POINT  Select the next magic point from Omega.
-%
-%    U_    := the function space (cell array of function handles)
-%    Omega := the evaluation domain, an (m x d) of m points in d dimensions
-%    s     := the magic points structure
+    s.Q = Q_all(s.x(1:jj), 1:jj);
+    assert(all(all(abs(triu(s.Q,1)) < 1e-9)));
  
-% Special case: if there are not yet any magic points use 
-%
-%    arg max_{u \in U_}   ||u(.)||_\infty
-%
-% ie. evaluate all elements of U_ at all points in the domain
-%     and pick the largest in magnitude.
-if isempty(s.q)
-    P = abs(apply(U_, Omega));
-    [~,idx] = max(P(:));
-    [r,k_worst] = ind2sub(size(Omega), idx);
-    x_i = Omega(r,:);
-    u_i = U_{k_worst};
-    return
-end
-
-% Find the worst fitting point across all interpolants and use this as
-% the next magic point.
-worst = 0;  k_worst = NaN;  x_i = NaN;  u_i = NaN;
-for k = 1:length(U_)
-    I_k = make_interpolant(U_{k}, s);
-    err_inf = abs(U_{k}(Omega) - I_k(Omega));
-    [p_max, idx] = max(err_inf);
-    if p_max > worst
-        worst = p_max;
-        k_worst = k;
-        u_i = U_{k};
-        x_i = Omega(idx,:);
+    % sanity checks
+    assert(abs(s.Q(jj,jj) - 1) < 1e-8);
+    assert(length(unique(s.u(1:jj))) == jj);
+ 
+    % update coefficients for each basis function.
+    % TODO: rank one update to linear system?
+    U_at_magic = U_all(s.x(1:jj), :);
+    for kk = 1:size(U_all,2)
+        beta_j_k = s.Q \ U_at_magic(:,kk);
+        I_j(:,kk) = sum(bsxfun(@times, beta_j_k', Q_all(:,1:jj)), 2);
     end
-end
-
-assert(~isnan(k_worst));
-
-
-
-function v_hat = make_interpolant(v, s)
-% MAKE_INTERPOLANT  Returns a *function* v_hat that interpolates v at the points x.
-%
-%    v :  The function to interpolate;  v : R^d -> R
-%    s :  The magic points interpolation structure 
-%
     
-m = length(s.q);
-assert(size(s.x,1) == m);
-assert(m > 0);
-
-% Special case: for a single interpolant there is no need 
-%               to compute any coefficients.
-% See p. 386 in [maday]
-if m == 1
-    v_hat = @(X) v(s.x(1,:))*s.q{1}(X);
-    return
+    % the active basis functions should interpolate themselves perfectly
+    Err = abs(U_all(:,s.u(1:jj)) - I_j(:,s.u(1:jj)));
+    assert(max(Err(:)) < 1e-8);
 end
 
 
-% First must obtain the b coefficients (denoted beta in [maday]).
-% Technically this does more work than needed since we throw away the
-% upper triangular part; revisit this later if it turns out to be a
-% performance issue.
-Q = apply(s.q, s.x);
 
-% These next two statements are not necessary; are entailed by algorithm.
-%Q = tril(Q);
-%Q(1:m+1:m*m) = 1;
-
-% see (3) in [maday]
-% This should be well-conditioned due to structure of Q.
-b = Q \ v(s.x);
-
-% see (2) in [maday]
+function [val, row, col] = ell_inf_2d(A)
+% MAX_IN_MATRIX  Finds the maximum magnitude value in a 2d matrix.
 %
-v_hat = @(X) sum(bsxfun(@times, b(:)', apply(s.q, X)), 2);
+%  This is just a bit of shorthand for clarity.
+[~, idx] = max(abs(A(:)));
+[row,col] = ind2sub(size(A), idx);
+val = A(row,col);
