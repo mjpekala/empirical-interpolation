bcb4f5f094af2ba6d42e295232ff39d650d4ae57
mjpekala <mike.pekala@jhuapl.edu>
2016-09-07 12:52:01 -0400

    cosmetic changes to plots

--- src/Tests/test_make_domain_1d.m
+++ src/Tests/test_make_domain_1d.m
@@ -2,8 +2,8 @@
 
 addpath('..')
 
-m_max = 30;
-[Omega, W_n] = make_domain_1d(m_max,.01);
+n_max = 30;
+[Omega, W_n] = make_domain_1d(n_max,.02);
 
 figure;
 plot(Omega, W_n{1}(Omega), ...
@@ -15,22 +15,23 @@ title('first four polynomial basis functions (1d)');
 
 
 % see figure 1 in [maday]
-lambdas = zeros(m_max,1);
-for m = 1:m_max
+lambdas = zeros(n_max,1);
+for m = 1:n_max
     [s, lambdas(m)] = choose_magic(Omega, W_n, m);
 end
 
 figure;
-plot(1:m_max, lambdas, '-o');
+plot(1:n_max, lambdas, '-o');
 xlabel('num. magic points');
 ylabel('$\Lambda_M$')
 
 figure;
+subplot(2,1,1);
 plot(s.Omega(s.x), ones(size(s.x)), 'o');
 title('distribution of magic points');
 xlabel('x');
 
-figure;
+subplot(2,1,2);
 plot(acos(s.Omega(s.x)), ones(size(s.x)), 'o');
 title('distribution of magic points');
 xlabel('cos^{-1}(x)');
