d95cbedc2edf6f26c1fe14f2b0e7f06390e1f449
mjpekala <mike.pekala@jhuapl.edu>
2016-09-08 13:35:42 -0400

    can now provide vector valued inputs if desired

--- src/implied_vol.m
+++ src/implied_vol.m
@@ -1,8 +1,7 @@
 function sigma = implied_vol(c, S0, K, r, t)
 % IMPLIED_VOL  Computes the implied volatility for a European call.
 %
-%  Parameters:
-%
+%  Parameters (either all scalar or all vectors of same length):
 %     c  : call option price at t0
 %     S0 : stock price at t0
 %     K  : strike price
@@ -12,13 +11,23 @@ function sigma = implied_vol(c, S0, K, r, t)
 %  References:
 %    Hull, "Options, Futures and Other Derivatives," fifth ed.
 
+if length(c) == 1
+    sigma = implied_vol_scalar(c, S0, K, r, t);
+else
+    sigma = zeros(length(c),1);
+    for ii = 1:length(c)
+        sigma(ii) = implied_vol_scalar(c(ii), S0(ii), K(ii), r(ii), t(ii));
+    end
+end
+
+
+
+function sigma = implied_vol_scalar(c, S0, K, r, t)
+% IMPLIED_VOL_SCALAR  Determines a single implied volatility value.
 
 % see 12.20, 12.21 in [hull]
 d1 = @(sigma) (log(S0/K) + (r + sigma^2/2)*t) / (sigma * sqrt(t));
 d2 = @(sigma) d1(sigma) - sigma * sqrt(t);
-
 f = @(sigma) S0*normcdf(d1(sigma)) - K*exp(-r*t)* normcdf(d2(sigma)) - c;
 
 sigma = fzero(f, 0.2);
-
-
