afdebfb323a8e5ee9eb7f44d7d49ee6ea2dd8af7
mjpekala <mike.pekala@jhuapl.edu>
2016-09-08 14:01:36 -0400

    added some comments

--- src/make_domain_2d.m
+++ src/make_domain_2d.m
@@ -2,6 +2,11 @@ function [Omega, x, y, idx] = make_domain_2d(delta, shape)
 % MAKE_DOMAIN_2D  Creates a simple 2d domain (compact subset 
 %                 of R^2) for interpolation.
 %
+%  Parameters:
+%    delta : (scalar) spacing in each dimension (assumed isotropic)
+%    shape : shape of the domain; see below for valid options.
+
+% mjp, sept 2016
 
 if nargin < 2, shape = 'square'; end
 if nargin < 1, delta = 0.02; end
