8e9c536f226e0a017948a99d0d16a8f815f48c8b
mjpekala <mike.pekala@jhuapl.edu>
2016-09-21 09:11:29 -0400

    added error_analysis_2d

--- src/demo_2d_iv.m
+++ src/demo_2d_iv.m
@@ -20,7 +20,17 @@ iv_scaled = @(X) implied_vol(1+c_min+((c_max - c_min)/2)*X(:,1), ...
                              ones(size(X,1),1)*t);
 
 n_max = 10;
-W_n = make_polynomial_basis(2, n_max);
-[Omega, domain_info] = make_domain_2d(delta, 'square');
 
-compare_methods_2d(iv_scaled, W_n, Omega, domain_info);
+if 0
+    W_n = make_polynomial_basis(2, n_max);
+    [Omega, domain_info] = make_domain_2d(delta, 'square');
+    compare_methods_2d(iv_scaled, W_n, Omega, domain_info);
+else
+    [m_vals, err_ell_inf] = error_analysis_2d(iv_scaled, n_max, .01);
+    figure;
+    plot(m_vals, err_ell_inf(:,1), 'o-', m_vals, err_ell_inf(:,2), 'o-');
+    legend('magic points', 'griddata');
+    xlabel('m'); ylabel('err ell_inf');
+    title('ell_infty error for implied vol example');
+end
+
