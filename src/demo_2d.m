c6a1ba49a4578cce8b68ccdf637efe234b811165
mjpekala <mike.pekala@jhuapl.edu>
2016-09-21 09:11:56 -0400

    added error_analysis_2d

--- src/demo_2d.m
+++ src/demo_2d.m
@@ -12,7 +12,17 @@ f = @(X) cos(3*X(:,1)) .* sin(2*X(:,2));
 f_rescaled = @(X) f(X*pi);
 
 n_max = 14;
-W_n = make_polynomial_basis(2, n_max);
-[Omega, domain_info] = make_domain_2d(.02, 'triangle');
 
-compare_methods_2d(f_rescaled, W_n, Omega, domain_info);
+if 0
+    W_n = make_polynomial_basis(2, n_max);
+    [Omega, domain_info] = make_domain_2d(.02, 'triangle');
+    compare_methods_2d(f_rescaled, W_n, Omega, domain_info);
+else
+    [m_vals, err_ell_inf] = error_analysis_2d(iv_scaled, n_max, .01);
+    figure;
+    plot(m_vals, err_ell_inf(:,1), 'o-', m_vals, err_ell_inf(:,2), 'o-');
+    legend('magic points', 'griddata');
+    xlabel('m'); ylabel('err ell_inf');
+    title('ell_infty error for implied vol example');
+end
+
